LineageOS/android_packages_overlays_Lineage

Implement hide gestural navigation hint bar [3/4]
Change-Id: I5ed5c55ec46c3bb5e91aeb88bd5f06bd4445c2f0
Signed-off-by: Jesse Chan <jc@lineageos.org>

customizations/NavigationBarNoHint/Android.mk
@@ -1,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := LineageNavigationBarNoHint
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := LineageNavigationBarNoHint
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+include $(BUILD_RRO_PACKAGE)

AfterlifeOS/packages_overlays_Afterlife

Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

navbar/NavigationBarNoHint/Android.mk
@@ -1,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := NavigationBarNoHint
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := NavigationBarNoHint
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+include $(BUILD_RRO_PACKAGE)